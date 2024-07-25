7851976176e84249ea347fabe4b1fbc9f4e4c621
Leonardo Luz <leonardoluz10@hotmail.com>
2024-07-25 18:05:05 -0300

    feat: :white_check_mark: Feat: Criando decorators escape e inspect.

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,27 +1,22 @@
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 
 export abstract class View<T> {
     protected elemento: HTMLElement
-    private escapar = false;
 
-    constructor(seletor: string, escapar?: boolean) {
+    constructor(seletor: string) {
         const elemento = document.querySelector(seletor);
         if (elemento) {
             this.elemento = elemento as HTMLElement;
         } else {
             throw new Error(`Seletor ${seletor} não existe no DOM. Verifique.`);
         }
-        if (escapar) {
-            this.escapar = escapar;
-        }
     }
 
-    @logarTempoDeExecucao()
+    @logarTempoDeExecucao(true)
+    @inspect
     public update(model: T): void {
-        const template = this.template(model)
-        if (this.escapar) {
-            template.replace(/<script>[\s\S]*?<\/script>/, '')
-        }
+        let template = this.template(model)
         this.elemento.innerHTML = template;
     }
 
